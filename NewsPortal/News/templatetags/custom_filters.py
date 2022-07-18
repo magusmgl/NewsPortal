c30db5daceac79c83c37a7283c7e0e4b00bd810f
Maksim Glazunov <m.glazunov@knopka.com>
2022-07-18 11:05:05 +0500

    fix custom_filter

--- NewsPortal/News/templatetags/custom_filters.py
+++ NewsPortal/News/templatetags/custom_filters.py
@@ -1,23 +1,17 @@
 from django import template
+import pymorphy2
 
 register = template.Library()
 
-@register.filter()
-def censor(text: str, censor_word=['спецоперации', 'релокации']) -> str:
-    words = text.strip()
+@register.filter(name='censor')
+def censor(text: str, censor_word=['спецоперации', 'релокации', 'компания', 'Россия']) -> str:
+    words = text.split()
     for i in range(len(words)):
-        if words[i] in censor_word or words[i] in map(lambda x: x.title(), censor_word):
+        if normalized_word(words[i].lower()) in censor_word:
             words[i] = "*" * len(words[i])
     return " ".join(words)
 
-
-
-
-
-
-
-
-
-
-
-    return f'{text}'
+def normalized_word(word:str):
+    word.strip()
+    morph = pymorphy2.MorphAnalyzer()
+    return morph.parse(word)[0].normal_form
