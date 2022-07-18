0e54ee9cb47216e322816b45554a669d1a2d3232
Maksim Glazunov <m.glazunov@knopka.com>
2022-07-18 10:04:54 +0500

    create views

--- NewsPortal/News/views.py
+++ NewsPortal/News/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
+class NewsList(ListView):
+    # model = Post
+    # ordering = 'post_date'
+    queryset = Post.objects.order_by('-date')
+    template_name = 'news_list.html'
+    context_object_name = 'news_list'
+
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    pk_url_kwarg = 'id'
