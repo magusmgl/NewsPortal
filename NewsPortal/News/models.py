d532d4648a26c59eb8cc423cfc518f4a4c5de1a0
Maksim Glazunov <m.glazunov@knopka.com>
2022-08-05 11:33:04 +0500

    add get_absolute_url for User models

--- NewsPortal/News/models.py
+++ NewsPortal/News/models.py
@@ -8,6 +8,10 @@ class User(AbstractUser):
     def __str__(self):
         return self.get_full_name()
 
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('profile')
+
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='author')
