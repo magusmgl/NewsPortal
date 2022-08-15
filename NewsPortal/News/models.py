01ce70cb4aafaa2459e7ed0c246c20dd8e10b13c
Maksim Glazunov <m.glazunov@knopka.com>
2022-08-15 22:51:20 +0500

    add models CategorySubcribes

--- NewsPortal/News/models.py
+++ NewsPortal/News/models.py
@@ -40,6 +40,7 @@ class Author(models.Model):
 
 class Category(models.Model):
     category_name = models.CharField(max_length=64, unique=True, db_column='name')
+    subscribers = models.ManyToManyField('User', through='CategorySubcribes', db_column='category')
 
     def __str__(self):
         return self.category_name.title()
@@ -119,3 +120,15 @@ class Comment(models.Model):
     def dislike(self):
         self.comment_rating -= 1
         self.save()
+
+
+class CategorySubcribes(models.Model):
+    """ Модель ПОДПИСКИ на КАТЕОРИЮ """
+    subcribe_user = models.ForeignKey('User',
+                                      null=True,
+                                      on_delete=models.SET_NULL,
+                                      verbose_name='Подписчик')
+    category = models.ForeignKey('Category',
+                                 null=True,
+                                 on_delete=models.SET_NULL,
+                                 verbose_name='Категория')
