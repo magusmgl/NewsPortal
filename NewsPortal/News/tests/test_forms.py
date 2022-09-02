d4b73ac0d253d03528b2d0e9dda1d3b8fee7aaa1
Maksim Glazunov <m.glazunov@knopka.com>
2022-09-02 10:06:59 +0500

    fix test_forms

--- NewsPortal/News/tests/test_forms.py
+++ NewsPortal/News/tests/test_forms.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from django.urls import reverse
 
 from News.forms import PostForm, User, Author, Category
 from News.models import User, Author, Category, Post
@@ -39,47 +38,25 @@ class PostFormTest(TestCase):
         self.assertTrue(form.fields['text'].label == None or form.fields['text'].label == 'Текст поста')
 
     def test_post_form_text_equal_to_title(self):
-        user = User.objects.create_user(username='ivan1234', first_name='ivan', last_name='ivianov',
-                                        email="ivan1234@mail.ru", password="1234")
-        author = Author.objects.create(user=user)
-        category = Category.objects.create(category_name='Политика')
         title = '_' * 150
         text = '_' * 150
 
-        form_data = {'category': [category], 'author': author, 'title': title, 'text': text, }
+        form_data = {'category': [self.category],
+                     'author': self.author,
+                     'title': title,
+                     'text': text,
+                     }
         form = PostForm(data=form_data)
         self.assertFalse(form.is_valid())
 
     def test_post_form_text_not_equal_to_title(self):
-        user = User.objects.create_user(username='ivan1234', first_name='ivan', last_name='ivianov',
-                                        email="ivan1234@mail.ru", password="1234")
-        author = Author.objects.create(user=user)
-        category = Category.objects.create(category_name='Политика')
         title = '_' * 15
         text = '_' * 200
 
-        form_data = {'category': [category], 'author': author, 'title': title, 'text': text, }
+        form_data = {'category': [self.category],
+                     'author': self.author,
+                     'title': title,
+                     'text': text,
+                     }
         form = PostForm(data=form_data)
         self.assertTrue(form.is_valid())
-
-    # def test_news_create(self):
-    #     response = self.client.post(
-    #         reverse('news_create'),
-    #         {
-    #             'author': self.author,
-    #             'category': self.category,
-    #             'title': 'New title',
-    #             'text': 'New text',
-    #         }
-    #     )
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertEqual(Post.objects.last().title, 'New title')
-    #     self.assertEqual(Post.objects.last().text, 'New text')
-    #
-    # def test_news_edit(self):
-    #     response = self.client.post(
-    #         reverse('news_update', args=1),
-    #         {
-    #             'title': 'Update title',
-    #             'text': 'Body text',
-    #         })
