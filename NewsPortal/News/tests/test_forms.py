d04fd8656ff6aafddf9e9805c927108ba303eba7
Maksim Glazunov <m.glazunov@knopka.com>
2022-09-02 09:30:48 +0500

    add test for views

--- NewsPortal/News/tests/test_forms.py
+++ NewsPortal/News/tests/test_forms.py
@@ -1,9 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 from News.forms import PostForm, User, Author, Category
+from News.models import User, Author, Category, Post
 
 
 class PostFormTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(username='testuser',
+                                                        first_name='testuser_name',
+                                                        last_name='testuser_last_name',
+                                                        email='test@email.com',
+                                                        password='secret')
+
+        cls.author = Author.objects.create(user=cls.user)
+        cls.category = Category.objects.create(category_name='Политика')
+
     def test_post_form_author_label(self):
         form = PostForm()
         self.assertTrue(form.fields['author'].label == None or form.fields['author'].label == 'Автор поста')
@@ -47,3 +61,25 @@ class PostFormTest(TestCase):
         form_data = {'category': [category], 'author': author, 'title': title, 'text': text, }
         form = PostForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+    # def test_news_create(self):
+    #     response = self.client.post(
+    #         reverse('news_create'),
+    #         {
+    #             'author': self.author,
+    #             'category': self.category,
+    #             'title': 'New title',
+    #             'text': 'New text',
+    #         }
+    #     )
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertEqual(Post.objects.last().title, 'New title')
+    #     self.assertEqual(Post.objects.last().text, 'New text')
+    #
+    # def test_news_edit(self):
+    #     response = self.client.post(
+    #         reverse('news_update', args=1),
+    #         {
+    #             'title': 'Update title',
+    #             'text': 'Body text',
+    #         })
