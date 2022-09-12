6e4a0c5016bb105e4ca027d34c9c913f3d919218
Maksim <magus.mgl@yandex.ru>
2022-09-12 21:21:03 +0500

    update news/urls.py

--- NewsPortal/NewsPortal/urls.py
+++ NewsPortal/NewsPortal/urls.py
@@ -18,7 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pages/', include('django.contrib.flatpages.urls')),
-    path('', include('News.urls')),
     path('accounts/', include('allauth.urls')),
+    path('news/', include('News.urls')),
+    path('articles/', include('News.urls')),
+    path('pages/', include('django.contrib.flatpages.urls')),
+
 ]
