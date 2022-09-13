48504949e562fdcf46bf8b0eef2f6216b884fd85
Maksim <magus.mgl@yandex.ru>
2022-09-13 11:13:46 +0500

    add comments in adminpanel

--- NewsPortal/News/admin.py
+++ NewsPortal/News/admin.py
@@ -3,9 +3,28 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Post, Category, Author
+from .models import (
+    User,
+    Post,
+    Category,
+    Author,
+    Comment,
+)
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Author)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
+admin.site.register(Comment)
